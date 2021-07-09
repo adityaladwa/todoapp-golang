747dc6f74f9edfa45addeae19ab6e61569017718
Aditya Ladwa <ladwa.aditya@gmail.com>
2021-07-09 20:16:34 +0530

    Added pagination to list param

--- api/todos.go
+++ api/todos.go
@@ -2,6 +2,7 @@ package api
 
 import (
 	"net/http"
+	"strconv"
 	"time"
 
 	"github.com/google/uuid"
@@ -19,11 +20,18 @@ type todoResponse struct {
 }
 
 func (s *Server) GetTodos(c *fiber.Ctx) error {
-	args := db.ListTodosParams{
-		Limit:  10,
-		Offset: 0,
+	pageSize, err := strconv.ParseInt(c.Query("page_size"), 10, 32)
+	if err != nil {
+		c.SendStatus(http.StatusUnprocessableEntity)
 	}
-
+	pageNo, err := strconv.ParseInt(c.Query("page_no"), 10, 32)
+	if err != nil {
+		c.SendStatus(http.StatusUnprocessableEntity)
+	}
+	args := db.ListTodosParams{
+		Limit:  int32(pageSize),
+		Offset: int32(pageNo),
+	}
 	todos, err := s.store.ListTodos(c.UserContext(), args)
 	if err != nil {
 		c.SendString("error")
@@ -32,12 +40,12 @@ func (s *Server) GetTodos(c *fiber.Ctx) error {
 
 	var todoResponse []todoResponse
 	for i := 0; i < len(todos); i++ {
-		todoResponse = append(todoResponse, mapTodoToResponse(todos[i]))
+		todoResponse = append(todoResponse, mapToResponse(todos[i]))
 	}
 	return c.JSON(todoResponse)
 }
 
-func mapTodoToResponse(t db.Todo) todoResponse {
+func mapToResponse(t db.Todo) todoResponse {
 	var description string
 	if t.Description.Valid {
 		description = t.Description.String
