fe42d9a2fddfb6cabeca68ef80310eec9ec8d7a3
Aditya Ladwa <ladwa.aditya@gmail.com>
2021-07-13 18:39:24 +0530

    Using viper for loading env configs

--- go.mod
+++ go.mod
@@ -6,6 +6,7 @@ require (
 	github.com/gin-gonic/gin v1.7.2
 	github.com/google/uuid v1.2.0
 	github.com/lib/pq v1.3.0
+	github.com/spf13/viper v1.8.1
 	github.com/stretchr/testify v1.7.0
 	golang.org/x/crypto v0.0.0-20210513164829-c07d793c2f9a // indirect
 	golang.org/x/sys v0.0.0-20210630005230-0f9fa26af87c // indirect
--- main.go
+++ main.go
@@ -2,27 +2,28 @@ package main
 
 import (
 	"database/sql"
+	"fmt"
 	"log"
 
 	_ "github.com/lib/pq"
 
 	"github.com/adityaladwa/todoapp/api"
+	"github.com/adityaladwa/todoapp/config"
 	db "github.com/adityaladwa/todoapp/db/sqlc"
 )
 
-const (
-	dbDriver      = "postgres"
-	dbSource      = "postgresql://adityaladwa:secret@localhost:5432/todoapp?sslmode=disable"
-	serverAddress = ":8080"
-)
-
 func main() {
-	conn, err := sql.Open(dbDriver, dbSource)
+	fmt.Println("Starting main...")
+	config, err := config.LoadConfig(".")
+	if err != nil {
+		log.Fatal("cannot load configuration", err)
+	}
+	conn, err := sql.Open(config.DBDriver, config.DBSource)
 	if err != nil {
 		log.Fatal("Cannot connect to database: ", err)
 	}
 	store := db.NewStore(conn)
 	server := api.NewServer(store)
 
-	server.Start(serverAddress)
+	server.Start(config.ServerAddress)
 }
